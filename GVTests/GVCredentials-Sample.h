2335476a549db9a7c7d3f9792b668ad8ff18b23c
Joey Gibson <joey@joeygibson.com>
2011-04-11 10:25:52 -0400

    Added sample credentials file for testing.

---
+++ GVTests/GVCredentials-Sample.h
@@ -0,0 +1,8 @@
+// Set these as appropriate for your Google Voice account,
+// and rename to GVCredentials.h
+//
+// DO NOT CHECK IN GVCredentials.h!!!
+
+#define USERNAME @"my_email_address@gmail.com"
+#define PASSWORD @"my_password"
+#define SOURCE @"my_company-my_product-1.0"
