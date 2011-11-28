f46c8e58f912b92dc88f0300d23e11760a9ee35a
Joey Gibson <joey@joeygibson.com>
2011-11-28 09:23:31 -0500

    Removed extra double-quote from GVCredentials-Sample.h that was causing problems.

--- GVTests/GVCredentials-Sample.h
+++ GVTests/GVCredentials-Sample.h
@@ -36,6 +36,6 @@
 #define PASSWORD @"my_password"
 #define SOURCE @"my_company-my_product-1.0"
 #define TEXT_PHONE_NUMBER @"4045551212"
-#define TEST_DESTINATION_NUMBER @"4045551212""
+#define TEST_DESTINATION_NUMBER @"4045551212"
 #define TEST_ACCOUNT_TYPE @"HOSTED"
 
